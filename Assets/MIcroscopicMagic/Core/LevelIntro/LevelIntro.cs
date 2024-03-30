8a0ca6f45f9634b4076535c5588bd69bb1913fcb
takiido <157808651+takiido@users.noreply.github.com>
2024-03-29 22:01:57 -0500

    added multiple fonts and layers; added text destructor

--- Assets/MIcroscopicMagic/Core/LevelIntro/LevelIntro.cs
+++ Assets/MIcroscopicMagic/Core/LevelIntro/LevelIntro.cs
@@ -9,24 +9,39 @@ public class LevelIntro : MonoBehaviour
     public Color txtColor;
 
     public Material bgMaterial;
-    public TMP_Text levelName;
+    public TMP_Text[] levelNames;
 
     public TMP_FontAsset firstFont;
+    public TMP_FontAsset altFirstFont;
     public TMP_FontAsset secondFont;
+    public TMP_FontAsset altSecondFont;
+
+    public float[] maxOpacities;
 
     private float timePassed = 0.0f;
     private float startTime;
     private float timeElapsed;
     private float lerpDuration = 3f;
-    private float startValue = 1.0f;
-    private float endValue = 0.0f;
+
+    private float _opacityVal = 1.0f;
 
     private void Start()
     {
         startTime = Time.time;
-        levelName.color = txtColor;
+
+        foreach (var levelName in levelNames)
+        {
+            levelName.color = txtColor;
+        }
+        
+        levelNames[0].alpha = maxOpacities[0];
+        levelNames[1].alpha = maxOpacities[1];
+        levelNames[2].alpha = maxOpacities[2];
+        levelNames[3].alpha = maxOpacities[3];
+        levelNames[4].alpha = maxOpacities[4];
+        
         bgMaterial.SetColor("_Background Color", bgColor);
-        bgMaterial.SetFloat("_Opacity", 1.0f);
+        bgMaterial.SetFloat("_Opacity", _opacityVal);
     }
 
     private void Update()
@@ -36,16 +51,38 @@ public class LevelIntro : MonoBehaviour
 
         if (timePassed > deltaTime)
         {
-            levelName.font = levelName.font == firstFont ? secondFont : firstFont;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 4)
+                {
+                    levelNames[i].font = levelNames[i].font == firstFont ? secondFont : firstFont;
+                }
+                else
+                {
+                    levelNames[i].font = levelNames[i].font == altFirstFont ? altSecondFont : altFirstFont;
+                }
+            }
             timePassed = 0f;
         }
 
         if (Time.time > startTime + 5f && timeElapsed < lerpDuration)
         {
-            float opacityVal = Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+            for (int i = 0; i < 5; i++)
+            {
+                _opacityVal = Mathf.Lerp(maxOpacities[i], 0.0f, timeElapsed / lerpDuration);
+                levelNames[i].alpha = _opacityVal;
+            }
             timeElapsed += Time.deltaTime;
-            bgMaterial.SetFloat("_Opacity", opacityVal);
-            levelName.alpha = opacityVal;
+            bgMaterial.SetFloat("_Opacity", _opacityVal);
         }
+
+        if (_opacityVal < 0.01f)
+        {
+            foreach (var levelName in levelNames)
+            {
+                Destroy(levelName.gameObject);
+            }
+            Destroy(gameObject);
+        } 
     }
 }
